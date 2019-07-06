f895fdaeb1201659009fadf020458e7379587c54
Pascal Havé <hpwxf@haveneer.com>
2019-07-06 22:05:28 +0200

    config: add clang-format target and Chronium like style

---
+++ cmake/ClangFormatProcess.cmake
@@ -0,0 +1,15 @@
+# --------------- runs clang-format in place using the style file ---------------
+
+if(LIBKRIGING_SOURCE_DIR AND CLANG_FORMAT)
+    # get C++ sources file list (ignoring packages)
+    file(GLOB_RECURSE ALL_SOURCE_FILES
+            ${LIBKRIGING_SOURCE_DIR}/src/**.[hc]pp
+            ${LIBKRIGING_SOURCE_DIR}/tests/**.[hc]pp
+            # ${LIBKRIGING_SOURCE_DIR}/bindings/R/**.[hc]pp
+            )
+
+    # apply style to the file list
+    foreach(SOURCE_FILE ${ALL_SOURCE_FILES})
+        execute_process(COMMAND "${CLANG_FORMAT}" -style=file -verbose -i "${SOURCE_FILE}")
+    endforeach()
+endif()
