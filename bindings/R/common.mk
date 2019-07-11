41ed11abd315b8911285fbca45fd76f842908e3c
Pascal Havé <hpwxf@haveneer.com>
2019-07-11 15:40:15 +0200

    config: add verification about required environment variables settings.
    * MAKE_SHARED_LIBS : defines what kind of library (static or dynamic) is used
    * LIBKRIGING_PATH : defines where is installed linKriging

---
+++ bindings/R/common.mk
@@ -0,0 +1,14 @@
+# MAKE_SHARED_LIBS and LIBKRIGING_PATH can be set
+# * using shell : export VAR=VALUE
+# * Using R     : Sys.setenv("VAR"="VALUE")
+check:
+ifeq ("${MAKE_SHARED_LIBS}","")
+	$(info empty MAKE_SHARED_LIBS env variable implies default behavior [on]")
+else ifeq ("${MAKE_SHARED_LIBS}","on")  # legal value
+else ifeq ("${MAKE_SHARED_LIBS}","off") # legal value
+else
+	$(error MAKE_SHARED_LIBS env variable must be set to on or off (not '${MAKE_SHARED_LIBS}')")
+endif
+ifeq ("${LIBKRIGING_PATH}","")
+	$(error LIBKRIGING_PATH env variable must be set using shell `export` or R `Sys.setenv`")
+endif
