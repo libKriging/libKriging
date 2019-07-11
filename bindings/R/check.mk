fc57450d2d8fb6995b56725e9f6e4f1c2efb0f65
Pascal Havé <hpwxf@haveneer.com>
2019-07-11 19:04:32 +0200

    build(R): add additional checks for Windows to verify that all environment variables are well set

--- bindings/R/check.mk
+++ bindings/R/check.mk
@@ -1,7 +1,7 @@
 # MAKE_SHARED_LIBS and LIBKRIGING_PATH can be set
 # * using shell : export VAR=VALUE
 # * Using R     : Sys.setenv("VAR"="VALUE")
-check:
+.check:
 ifeq ("${MAKE_SHARED_LIBS}","")
 	$(info empty MAKE_SHARED_LIBS env variable implies default behavior [on]")
 else ifeq ("${MAKE_SHARED_LIBS}","on")  # legal value
@@ -11,4 +11,9 @@ else
 endif
 ifeq ("${LIBKRIGING_PATH}","")
 	$(error LIBKRIGING_PATH env variable must be set using shell `export` or R `Sys.setenv`")
+endif
+ifeq ("$(OS)","Windows_NT")
+ifeq ("${EXTRA_SYSTEM_LIBRARY_PATH}","")
+	$(error EXTRA_SYSTEM_LIBRARY_PATH env variable must be set using shell `export` or R `Sys.setenv`")
+endif
 endif
