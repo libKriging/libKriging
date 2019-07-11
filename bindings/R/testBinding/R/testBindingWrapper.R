a8cafc4e408e7da2de1b6872ebd68aa4bcabf5ce
Pascal Havé <hpwxf@haveneer.com>
2019-07-11 15:36:44 +0200

    config(R) : rename manual binding to avoid collision with generated ones

--- bindings/R/testBinding/R/testBindingWrapper.R
+++ bindings/R/testBinding/R/testBindingWrapper.R
@@ -1,10 +1,10 @@
 # wrapper function to invoke test_binding
-test_binding <- function() {
+test_binding_1 <- function() {
   result <- .Call("test_binding")
   return(result)
 }
 
-test_binding2 <- function() {
+test_binding_2 <- function() {
   result <- .Call("test_binding2")
   return(result)
 }
