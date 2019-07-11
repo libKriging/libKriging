22ba19644f5d93df64f9dc71622db756b7983590
Pascal Havé <hpwxf@haveneer.com>
2019-07-11 15:55:16 +0200

    build(R) : a complete example using R matrices to libKriging core

--- bindings/R/testBinding/test/test.R
+++ bindings/R/testBinding/test/test.R
@@ -1,10 +1,9 @@
 library(testBinding)
-test_binding()
-test_binding2()
+test_binding_1()
+test_binding_2()
 
 
-# set.seed(42)
-# # X <- matrix(rnorm(4*4), 4, 4)
-# # Z <- X %*% t(X)
-# #
-# # getEigenValues(Z)
+ set.seed(42)
+X <- matrix(rnorm(4*4), 4, 4)
+Z <- X %*% t(X)
+getEigenValues(Z)
--- src/lib/include/libKriging/ArmadilloTestClass.hpp
+++ src/lib/include/libKriging/ArmadilloTestClass.hpp
@@ -6,12 +6,13 @@
 #define LIBKRIGING_ARMADILLOTESTCLASS_HPP
 
 #include "libKriging_exports.h"
+#include <armadillo>
 
 class ArmadilloTestClass {
  public:
   LIBKRIGING_EXPORT ArmadilloTestClass();
+
+  static arma::vec getEigenValues(arma::mat M) { return arma::eig_sym(M); }
 };
-
-
 
 #endif //LIBKRIGING_ARMADILLOTESTCLASS_HPP
