73f1809ceb6b183146230fb9eb19f54fc8704208
Pascal Havé <hpwxf@haveneer.com>
2019-08-14 11:54:35 +0200

    test: add for DemoArmadilloClass to improve code coverage

--- tests/armadillo_example.cpp
+++ tests/armadillo_example.cpp
@@ -3,4 +3,11 @@
 
 int main() {
   std::unique_ptr<DemoArmadilloClass> x(new DemoArmadilloClass());
+  x->test();
+
+  const int n = 40;
+  arma::mat X(n, n, arma::fill::randn);
+  arma::mat Z = X* X.t();
+  arma::vec ev = x->getEigenValues(Z);
+  std::cout << "Eigen vectors" << ev << std::endl;
 }
