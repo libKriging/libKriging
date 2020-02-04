1e07bca669b072e1ebce2d3814ab9296eaac2a3b
yannrichet <yannrichet@laposte.net>
2020-02-04 15:24:09 +0100

    link inside R binding

--- bindings/R/rlibkriging/src/regressions_rcpp_binding.cpp
+++ bindings/R/rlibkriging/src/regressions_rcpp_binding.cpp
@@ -8,3 +8,13 @@ Rcpp::List linear_regression(arma::vec y, arma::mat X) {
   return Rcpp::List::create(Rcpp::Named("coefficients") = std::get<0>(ans),
                             Rcpp::Named("stderr") = std::get<1>(ans));
 }
+
+#include "libKriging/OrdinaryKriging.hpp"
+
+// [[Rcpp::export]]
+Rcpp::List ordinary_kriging(arma::vec y, arma::mat X,arma::vec theta) {
+    OrdinaryKriging ok;
+    auto ans = ok.fit(y, X, theta);
+    return Rcpp::List::create(Rcpp::Named("gamma") = std::get<0>(ans),
+                              Rcpp::Named("theta") = std::get<1>(ans));
+}
