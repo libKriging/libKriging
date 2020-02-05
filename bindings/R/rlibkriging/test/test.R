6957a57f75cd55421412dfb0e966d1ca95f5e520
yannrichet <yannrichet@laposte.net>
2020-02-05 11:20:12 +0100

    feature: add R object API
    use (implicit) method predict.LinearRegression for predict(rl,x)

--- bindings/R/rlibkriging/test/test.R
+++ bindings/R/rlibkriging/test/test.R
@@ -13,6 +13,18 @@ getEigenValues(obj)
 rm(obj)
 gc()
 
+n <- 10
+X <- as.matrix(runif(n))
+y = 4*X+rnorm(n,0,.1)
+r <- linear_regression(y, X)
+plot(X,y)
+x=as.matrix(seq(0,1,,100))
+px = predict.LinearRegression(r,x)
+lines(x,px$y)
+lines(x,px$y-2*px$stderr,col='red')
+lines(x,px$y+2*px$stderr,col='red')
+
+
 ncol <- 3
 nrow <- 4
 X <- matrix(nrow=nrow, ncol=ncol)
@@ -20,5 +32,7 @@ X[,2:ncol] = matrix(rexp(nrow*(ncol-1)))
 X[,1] = 1
 sol <- rexp(ncol)
 y <- X %*% sol
-ans <- linear_regression(y, X)
-norm(y - X %*% ans$coefficients)
+r <- linear_regression(y, X)
+(y - predict.LinearRegression(r,X)$y)/predict.LinearRegression(r,X)$stderr
+
+
