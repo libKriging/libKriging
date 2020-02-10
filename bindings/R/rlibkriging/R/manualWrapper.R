611141960191f5de00ad6bd9b18d8184de7ef362
yannrichet <yannrichet@laposte.net>
2020-02-10 14:36:47 +0100

    use (implicit) method predict.LinearRegression for predict(rl,x)

--- bindings/R/rlibkriging/R/manualWrapper.R
+++ bindings/R/rlibkriging/R/manualWrapper.R
@@ -9,5 +9,19 @@ demo_binding2 <- function() {
   return(result)
 }
 
-predict.LinearRegression <- function(object,X)
-    return(linear_regression_predict(object, X))
+predict.LinearRegression <- function(object,x) {
+    return(linear_regression_predict(object, x))
+}
+
+predict <- function (x, ...) {
+    UseMethod("predict", x)
+}
+
+# n <- 10
+# X <- as.matrix(runif(n))
+# y = 4*X+rnorm(n,0,.1)
+# r <- linear_regression(y, X)
+# plot(X,y)
+# x=as.matrix(seq(0,1,,100))
+# px = predict(r,x)
+# lines(x,px$y)
