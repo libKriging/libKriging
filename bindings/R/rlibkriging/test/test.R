5dbdaf70cf6763c55c22c88ded1e920dc44869d8
yannrichet <yannrichet@laposte.net>
2020-02-10 14:08:38 +0100

    ...

--- bindings/R/rlibkriging/test/test.R
+++ bindings/R/rlibkriging/test/test.R
@@ -9,6 +9,18 @@ Z <- X %*% t(X)
 getEigenValues(Z)
 
 
+n <- 10
+X <- as.matrix(runif(n))
+y = 4*X+rnorm(n,0,.1)
+r <- linear_regression(y, X)
+plot(X,y)
+x=as.matrix(seq(0,1,,100))
+px = predict.LinearRegression(r,x)
+lines(x,px$y)
+lines(x,px$y-2*px$stderr,col='red')
+lines(x,px$y+2*px$stderr,col='red')
+
+
 ncol <- 3
 nrow <- 4
 X <- matrix(nrow=nrow, ncol=ncol)
@@ -18,3 +30,5 @@ sol <- rexp(ncol)
 y <- X %*% sol
 r <- linear_regression(y, X)
 (y - predict.LinearRegression(r,X)$y)/predict.LinearRegression(r,X)$stderr
+
+
