e473a5fcc7b9762b4200dae49346f5b9b067fdb2
Pascal Havé <hpwxf@haveneer.com>
2020-02-11 19:30:10 +0100

    config(R): avoid package installation from source on Windows and macOS
    Sometimes binaries are not yet available since new sources are there, and it does'nt compile.
    
    Add an alternate solution using devtools package in comment.

--- bindings/R/rlibkriging/install/install.R
+++ bindings/R/rlibkriging/install/install.R
@@ -1,4 +1,17 @@
+# install.packages doc
+# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/install.packages
+
+# To install a specified version, you can use package 'devtools' (https://cran.r-project.org/web/packages/devtools) using
+#$ install.packages("devtools", repos=repos)
+#$ require(devtools)
+#$ install_version("ggplot2", version = "0.9.1", repos = "http://cran.us.r-project.org")
+
 repos <- 'http://cran.us.r-project.org'
-install.packages("Rcpp", repos=repos)
-install.packages("RcppArmadillo", repos=repos)
-install.packages("RhpcBLASctl", repos=repos)
+type <- switch(Sys.info()[['sysname']],
+               Windows= {"binary"},
+               Linux  = {"source"},
+               Darwin = {"binary"})
+
+install.packages("Rcpp", repos=repos, type=type)
+install.packages("RcppArmadillo", repos=repos, type=type)
+install.packages("RhpcBLASctl", repos=repos, type=type)
