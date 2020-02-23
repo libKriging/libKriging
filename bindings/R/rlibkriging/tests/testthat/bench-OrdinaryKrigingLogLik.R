84631ba4770ea8349d1bcd42c1ac906b4abc756c
yannrichet <richetyann@gmail.com>
2020-02-23 22:14:00 +0100

    seems cpp is 2x faster for ll, and same speed for grad ll

--- bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingLogLik.R
+++ bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingLogLik.R
@@ -1,28 +1,30 @@
 library(testthat)
 
 f = function(X) apply(X,1,function(x) sum(x^2))
-n <- 2000
+n <- 100
 set.seed(123)
-X <- cbind(runif(n),runif(n),runif(n))
+X <- cbind(runif(n),runif(n),runif(n),runif(n))
 y = f(X)
 
+N = 10
+
 xx = seq(0.01,1,,11)
-times = list(R_ll=rep(NA,length(xx)),R_gll=rep(NA,length(xx)),cpp_ll=rep(NA,length(xx)),cpp_gll=rep(NA,length(xx)))
+times = list(R_ll=rep(0,length(xx)),R_gll=rep(0,length(xx)),cpp_ll=rep(0,length(xx)),cpp_gll=rep(0,length(xx)))
 
 
 k = DiceKriging::km(design=X,response=y,covtype = "gauss")
 i=1
 for (x in xx){
   envx = new.env()
-  times$R_ll[i]=system.time(llx <- DiceKriging::logLikFun(rep(x,3),k,envx))
-  times$R_gll[i]=system.time(gllx <- DiceKriging::logLikGrad(rep(x,3),k,envx))
+  for (n in 1:N) times$R_ll[i]=times$R_ll[i]+system.time(llx <- DiceKriging::logLikFun(rep(x,ncol(X)),k,envx))
+  for (n in 1:N) times$R_gll[i]=times$R_gll[i]+system.time(gllx <- DiceKriging::logLikGrad(rep(x,ncol(X)),k,envx))
   i=i+1
 }
 
 r <- ordinary_kriging(y, X)
 i=1
 for (x in xx){
-  times$cpp_ll[i]=system.time(ll2x <- ordinary_kriging_loglikelihood(r,rep(x,3)))
-  times$cpp_gll[i]=system.time(gll2x <- ordinary_kriging_loglikelihoodgrad(r,rep(x,3)))
+  for (n in 1:N) times$cpp_ll[i]=times$cpp_ll[i]+system.time(ll2x <- ordinary_kriging_loglikelihood(r,rep(x,ncol(X))))
+  for (n in 1:N) times$cpp_gll[i]=times$cpp_gll[i]+system.time(gll2x <- ordinary_kriging_loglikelihoodgrad(r,rep(x,ncol(X))))
   i=i+1
 }
