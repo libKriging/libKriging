3d4c5a7fcb25b629dcb59db2d29729d9ab4c93a4
Pascal Havé <hpwxf@haveneer.com>
2020-02-28 11:18:43 +0100

    fix(test,R): fix accuracy error in relative error for linear regression [skip ci]

--- bindings/R/rlibkriging/tests/testthat/test-LinearRegression.R
+++ bindings/R/rlibkriging/tests/testthat/test-LinearRegression.R
@@ -1,8 +1,26 @@
 library(testthat)
 
+set.seed(123)
+
+precision <- 1e-14
+
+ncol <- 3
+nrow <- 4
+X <- matrix(nrow=nrow, ncol=ncol)
+X[,2:ncol] = matrix(rexp(nrow*(ncol-1)))
+X[,1] = 1
+sol <- rexp(ncol)
+y <- X %*% sol
+r <- linear_regression(y, X)
+# the following tests should work with it, since the computations are analytical
+test_that(desc="Predict linear reg. is exact on analytic solution",
+          expect_true(relative_error(predict.LinearRegression(r,X)$y,y) < precision))
+
+noise_amplitude <- 0.1
+
 n <- 10
 X <- as.matrix(runif(n))
-y = 4*X+rnorm(n,0,.1)
+y = 4*X+rnorm(n,0,noise_amplitude)
 r <- linear_regression(y, X)
 plot(X,y)
 x=as.matrix(seq(0,1,,100))
@@ -12,6 +30,7 @@ lines(x,px$y)
 lines(x,px$y-2*px$stderr,col='red')
 lines(x,px$y+2*px$stderr,col='red')
 
-precision <- 1e-1  # the following tests should work with it, since the computations are analytical
-test_that(desc="Predict linear reg. is exact on the design points", 
-          expect_true(relative_error(predict.LinearRegression(r,X)$y,y) < precision))
+test_that(desc="Predict linear reg. is exact on the design points",
+          expect_true(relative_error(predict.LinearRegression(r,X)$y,y) < 10 * precision + 10 * noise_amplitude))
+
+
