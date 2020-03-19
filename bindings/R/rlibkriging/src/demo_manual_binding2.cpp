9242283f7d742a0a35bc7b7e2172da881f2f6f7c
Pascal H <hpwxf@haveneer.com>
2020-03-19 21:00:24 +0100

    reformat: fix format for all files (including R binding) (#33)
    * add guard to prevent RcppArmadillo to be included before other includes

--- bindings/R/rlibkriging/src/demo_manual_binding2.cpp
+++ bindings/R/rlibkriging/src/demo_manual_binding2.cpp
@@ -3,29 +3,26 @@
  ***************************************************************************/
 
 // Required since libKriging.hpp has signatures with armadillo types
-#include <RcppArmadillo.h>
-
 #include <R.h>
+#include <RcppArmadillo.h>
 #include <Rdefines.h>
 
 // FIXME: collision with Rinternals
 //#include <iostream>
 
+#include <libKriging/demo/DemoArmadilloClass.hpp>
 #include <memory>
 
-#include <libKriging/demo/DemoArmadilloClass.hpp>
-
 extern "C" SEXP demo_binding2() {
+  //    std::cout << "libKriging class tests" << std::endl;
+  printf("libKriging class tests\n");
 
-    //    std::cout << "libKriging class tests" << std::endl;
-    printf("libKriging class tests\n");
+  SEXP result;
 
-    SEXP result;
+  PROTECT(result = NEW_INTEGER(2));
+  INTEGER(result)[0] = (int)1;
+  INTEGER(result)[1] = (int)2;
+  UNPROTECT(1);
 
-    PROTECT(result = NEW_INTEGER(2));
-    INTEGER(result)[0] = (int) 1;
-    INTEGER(result)[1] = (int) 2;
-    UNPROTECT(1);
-
-    return (result);
+  return (result);
 }
