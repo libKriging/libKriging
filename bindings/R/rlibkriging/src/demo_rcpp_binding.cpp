03bc9afeb484941a7759a3812558ea239f8fb190
Pascal Havé <hpwxf@haveneer.com>
2020-03-19 14:14:29 +0100

    Reformat

--- bindings/R/rlibkriging/src/demo_rcpp_binding.cpp
+++ bindings/R/rlibkriging/src/demo_rcpp_binding.cpp
@@ -25,5 +25,4 @@ arma::vec getEigenValues(Rcpp::List obj) {
   return impl_ptr->getEigenValues();
 }
 
-
 // TODO try to use modul for implicit access : http://www.deanbodenham.com/learn/rcpp-classes-part-2.html
