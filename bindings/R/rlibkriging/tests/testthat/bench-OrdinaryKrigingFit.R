f412fd2a42b00abca336a7e71657b1e2454496be
Pascal H <hpwxf@haveneer.com>
2020-05-27 20:03:46 +0200

    fix (R) : update R tests with updated signature and nicer plots (#50)
    - add missing kernel argument
    - add grid
    - fix assignment syntax (prefers <- to =)

--- bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingFit.R
+++ bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingFit.R
@@ -1,10 +1,10 @@
 library(foreach)
 registerDoSEQ()
 
-f = function(X) apply(X,1,function(x) sum((x-.5)^2))
+f <- function(X) apply(X, 1, function(x) sum((x-.5)^2))
 
-logn = seq(1,2,by=.1)
-times = list(R=rep(NA,length(logn)),cpp=rep(NA,length(logn)))
+logn <- seq(1, 2, by=.1)
+times <- list(R=rep(NA, length(logn)), cpp=rep(NA, length(logn)))
 
 for (i in 1:length(logn)) {
   n <- floor(10^logn[i])
@@ -13,7 +13,7 @@ for (i in 1:length(logn)) {
   print(n)
   set.seed(123)
   X <- matrix(runif(n*d),ncol=d)
-  y = f(X)
+  y <- f(X)
   
   times$R[i] = system.time(
     try(k <- DiceKriging::km(design=X,response=y,covtype = "gauss", multistart = 10,control = list(trace=T,maxit=10), lower=rep(0.001,d),upper=rep(2*sqrt(d),d)))
@@ -23,12 +23,12 @@ for (i in 1:length(logn)) {
     try(r <- ordinary_kriging(y, X,"gauss"))
   )[1]
   
-  ll_cpp = ordinary_kriging_loglikelihood(r,ordinary_kriging_model(r)$theta)
-  e = new.env()
-  ll_R = DiceKriging::logLikFun(k@covariance@range.val,k,e)
+  ll_cpp <- ordinary_kriging_loglikelihood(r, ordinary_kriging_model(r)$theta)
+  e <- new.env()
+  ll_R <- DiceKriging::logLikFun(k@covariance@range.val, k, e)
   
-  gll_cpp = ordinary_kriging_loglikelihoodgrad(r,ordinary_kriging_model(r)$theta)
-  gll_R = DiceKriging::logLikGrad(k@covariance@range.val,k,e)
+  gll_cpp <- ordinary_kriging_loglikelihoodgrad(r, ordinary_kriging_model(r)$theta)
+  gll_R <- DiceKriging::logLikGrad(k@covariance@range.val, k, e)
   
   if (abs(ll_cpp - DiceKriging::logLikFun(param=as.numeric(ordinary_kriging_model(r)$theta),model=k))/ll_cpp>.1)
     stop("LL function is not the same bw DiceKriging/libKriging: ",ordinary_kriging_loglikelihood(r,ordinary_kriging_model(r)$theta)," vs. ",DiceKriging::logLikFun(param=as.numeric(ordinary_kriging_model(r)$theta),model=k))  
@@ -38,7 +38,7 @@ for (i in 1:length(logn)) {
   
 }
 
-plot(floor(10^logn),log(times$R),ylim=c(log(min(min(times$R,na.rm = T),min(times$cpp,na.rm = T))),log(max(max(times$R,na.rm = T),max(times$cpp,na.rm = T)))),xlab="nb points",ylab="log(user_time (s))")
+plot(floor(10^logn),log(times$R),ylim=c(log(min(min(times$R,na.rm = T),min(times$cpp,na.rm = T))),log(max(max(times$R,na.rm = T),max(times$cpp,na.rm = T)))),xlab="nb points",ylab="log(user_time (s))", panel.first=grid())
 text(20,-1,"DiceKriging")
 points(floor(10^logn),log(times$cpp),col='red')
 text(80,-1,"libKriging",col = 'red')
--- bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingLogLikFun.R
+++ bindings/R/rlibkriging/tests/testthat/bench-OrdinaryKrigingLogLikFun.R
@@ -1,12 +1,12 @@
 library(testthat)
 
-f = function(X) apply(X,1,function(x) sum(x^2))
+f <- function(X) apply(X, 1, function(x) sum(x^2))
 
-logn = seq(1,2.5,by=.1)
-times = list(R=rep(NA,length(logn)),cpp=rep(NA,length(logn)))
-N = 1000
+logn <- seq(1, 2.5, by=.1)
+times <- list(R=rep(NA, length(logn)), cpp=rep(NA, length(logn)))
+N <- 1000
 
-theta=0.5
+theta <- 0.5
 
 for (i in 1:length(logn)) {
   n <- floor(10^logn[i])
@@ -15,16 +15,16 @@ for (i in 1:length(logn)) {
   print(n)
   set.seed(123)
   X <- matrix(runif(n*d),ncol=d)
-  y = f(X)
+  y <- f(X)
 
-  k = DiceKriging::km(design=X,response=y,covtype = "gauss")
-  llx=0
+  k <- DiceKriging::km(design=X, response=y, covtype = "gauss")
+  llx <- 0
   times$R[i] = system.time(
     try({for (t in 1:N) llx <- llx+DiceKriging::logLikFun(rep(theta,ncol(X)),k)})
   )[1]
-  llx=llx/N
+  llx <- llx/N
 
-  r <- ordinary_kriging(y, X)
+  r <- ordinary_kriging(y, X,"gauss")
   times$cpp[i] = system.time(
     try({ll2x <- bench_loglik(N,r,rep(theta,ncol(X)))})
     # try({for (t in 1:N) ll2x <- ordinary_kriging_loglikelihood(r,rep(theta,ncol(X)))}) # Loop should be done inside c++, not from R...
@@ -34,6 +34,6 @@ for (i in 1:length(logn)) {
 }
 
 plot(main = "1000 logLik",floor(10^logn),log(times$R),col='black',ylim=c(log(min(min(times$R),min(times$cpp))),log(max(max(times$R),max(times$cpp)))),xlab="nb points",ylab="log(user_time (s))", panel.first=grid())
-text(20,0,"R",col='black')
+text(20,0,"DiceKriging",col='black')
 points(floor(10^logn),log(times$cpp),col='red')
 text(80,0,"C++",col = 'red')
