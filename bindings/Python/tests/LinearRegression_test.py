293e15e39898b3031b609a79ec6eff56afc04441
Pascal Havé <hpwxf@haveneer.com>
2020-07-13 17:24:04 +0200

    test(Python): add more tests

--- bindings/Python/tests/LinearRegression_test.py
+++ bindings/Python/tests/LinearRegression_test.py
@@ -1,11 +1,11 @@
 import pylibkriging as lk
 import numpy as np
+import pytest
 
 
-def test_linear_regression():
-    n = 40
-    m = 3
-
+@pytest.mark.parametrize("n", [40, 100, 1000])
+@pytest.mark.parametrize("m", [3, 6])
+def test_linear_regression_exact(n, m):
     sol = np.asarray(np.random.normal(size=(m, 1)), dtype=np.float64, order='F')
     X = np.asarray(np.random.normal(size=(n, m)), dtype=np.float64, order='F')
     X[:, 0] = 1
@@ -17,4 +17,27 @@ def test_linear_regression():
     y2, _stderr = rl.predict(X)
 
     eps = 1e-5
-    assert np.linalg.norm(y - y2, ord=2) <= eps
+    assert np.linalg.norm(y - y2, ord=np.inf) <= eps
+
+
+@pytest.mark.parametrize("n", [40, 100, 1000])
+@pytest.mark.parametrize("m", [3, 6])
+def test_linear_regression_noisy(n, m):
+    sol = np.asarray(np.random.normal(size=(m, 1)), dtype=np.float64, order='F')
+    X = np.asarray(np.random.normal(size=(n, m)), dtype=np.float64, order='F')
+    X[:, 0] = 1
+    y = X.dot(sol)  # or X @ sol
+
+    e = 1e-8
+    noiser = lambda x: x * np.random.normal(1, e)
+    y = np.vectorize(noiser)(y)
+    # y = np.array([noiser(v) for v in y])
+    # y = np.array(list(map(noiser, y)))
+
+    rl = lk.LinearRegression()
+    rl.fit(y, X)
+
+    y2, _stderr = rl.predict(X)
+
+    eps = 1e-5
+    assert np.linalg.norm(y - y2, ord=np.inf) <= eps
