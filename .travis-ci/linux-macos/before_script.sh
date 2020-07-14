1481bd1e1db79dd578106dcd7a94c239cce077b1
Pascal Havé <hpwxf@haveneer.com>
2020-07-14 19:19:00 +0200

    build: pretty output when display version of useful tools

--- .travis-ci/linux-macos/before_script.sh
+++ .travis-ci/linux-macos/before_script.sh
@@ -5,26 +5,26 @@ if [[ "$DEBUG_CI" == true ]]; then
     echo "PATH=$PATH"
 
     echo "C++ config: $(command -v c++)"
-    c++ --version
+    c++ --version | sed 's/^/  /'
     
     echo "CMake config: $(command -v cmake)"
-    cmake --version
+    cmake --version | sed 's/^/  /'
     
     if ( command -v octave >/dev/null 2>&1 ); then
       echo "Octave config: $(command -v octave)"
-      octave --version
+      octave --version | sed 's/^/  /'
       
     fi
 
     if ( command -v R >/dev/null 2>&1 ); then
       echo "R config: $(command -v R)"
-      R --version
+      R --version | sed 's/^/  /'
        
     fi
 
     if ( command -v python3 >/dev/null 2>&1 ); then
       echo "Python3 config: $(command -v python3)"
-      python3 --version
+      python3 --version | sed 's/^/  /'
     fi
 
     echo "EXTRA_CMAKE_OPTIONS = ${EXTRA_CMAKE_OPTIONS}"
