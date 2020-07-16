baced11f96ed725362c1ed7f2901edf1429a12ad
Pascal Havé <hpwxf@haveneer.com>
2020-07-16 16:01:17 +0200

    build: force activation of tested bindings

--- .travis-ci/octave-linux-macos/build.sh
+++ .travis-ci/octave-linux-macos/build.sh
@@ -8,6 +8,8 @@ fi
 BASEDIR=$(dirname "$0")
 BASEDIR=$(readlink -f "${BASEDIR}")
 
+[[ ${ENABLE_OCTAVE_BINDING} == "on" ]] || (echo "Octave binding must be 'on'" && exit 1 )
+
 CC=gcc-7 CXX=g++-7 \
-    EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=off -DENABLE_OCTAVE_BINDING=on -DDETECT_HDF5=false ${EXTRA_CMAKE_OPTIONS}" \
+    EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=off -DDETECT_HDF5=false ${EXTRA_CMAKE_OPTIONS}" \
     "${BASEDIR}"/../linux-macos/build.sh
