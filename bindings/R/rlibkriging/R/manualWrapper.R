58f794e122380b275874ae149b862a45530ba1af
Pascal Havé <hpwxf@haveneer.com>
2020-10-27 17:33:22 +0100

    Fix warning around manual wrapper

--- bindings/R/rlibkriging/R/manualWrapper.R
+++ bindings/R/rlibkriging/R/manualWrapper.R
@@ -1,10 +1,10 @@
 # wrapper function to invoke test_binding
-demo_binding1 <- function() {
+manual_demo_binding1 <- function() {
   result <- .Call("demo_binding1")
   return(result)
 }
 
-demo_binding2 <- function() {
+manual_demo_binding2 <- function() {
   result <- .Call("demo_binding2")
   return(result)
 }
--- bindings/R/rlibkriging/tests/testthat/test-Armadillo.R
+++ bindings/R/rlibkriging/tests/testthat/test-Armadillo.R
@@ -1,5 +1,8 @@
-demo_binding1()
-demo_binding2()
+manual_demo_binding1()
+manual_demo_binding2()
+
+demo_binding()
+demo_binding()
 
 set.seed(42)
 X <- matrix(rnorm(4*4), 4, 4)
