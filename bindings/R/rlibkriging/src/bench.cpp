fd6df3b92cfe4ccc099b6a7a4a3f5191fa355778
Pascal Havé <hpwxf@haveneer.com>
2020-10-29 10:36:52 +0100

    Add benchmark to show effect of Cholesky algorithm on system time measure

--- bindings/R/rlibkriging/src/bench.cpp
+++ bindings/R/rlibkriging/src/bench.cpp
@@ -5,7 +5,9 @@
 
 #include "libKriging/Bench.hpp"
 #include "libKriging/OrdinaryKriging.hpp"
+#include "libKriging/LinearRegression.hpp"
 #include <chrono>
+#include <ctime>
 
 // [[Rcpp::export]]
 arma::mat bench_solvetri(int n, arma::mat X, arma::vec y) {
@@ -84,4 +86,43 @@ arma::vec grad_optim(arma::vec x0) {
   Bench* b = new Bench(1);
   Rcpp::XPtr<Bench> impl_ptr(b);
   return impl_ptr->RosenbrockGrad(x0);
+}
+
+// [[Rcpp::export]]
+Rcpp::List bench_rcpp_link(const arma::vec &y,
+                           const arma::mat &X,
+                           int n0step,
+                           int n1step) {
+  auto start = std::chrono::system_clock::now();
+
+  arma::uword n = X.n_rows;
+  arma::mat Xtnorm = trans(X);
+  arma::mat R0(n,n);
+  for (arma::uword k = 0; k < n0step; k++) {
+    for (arma::uword j = 0; j < n; j++) {
+      for (arma::uword i = 0; i < j; i++) {
+        R0(i, j) = 0;
+      }
+    }
+    for (arma::uword j = 0; j < n; j++) {
+      for (arma::uword i = j; i < n; i++) {
+        auto&& diff = Xtnorm.col(i) - Xtnorm.col(j);
+        const double temp = arma::dot(diff, diff);
+        R0(i, j) = exp(-0.5 * temp);
+      }
+    }
+  }
+
+  arma::mat R = arma::symmatl(R0);
+  R.diag().ones();
+  for (arma::uword k = 0; k < n1step; k++) {
+    arma::mat T = chol(R);
+  }
+
+  auto end = std::chrono::system_clock::now();
+  double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+  elapsed /= 1e6;
+  Rcpp::List obj;
+  obj.attr("time") = elapsed;
+  return obj;
 }
---
+++ bindings/R/rlibkriging/tests/testthat/bench-Rcpp.R
@@ -0,0 +1,52 @@
+library(rlibkriging)
+
+f <- function(X) apply(X, 1, function(x) prod(sin((x-.5)^2)))
+
+logn <- seq(1.1, 2.5, by=.1)
+times <- list(chol_cpp_user=rep(NA, length(logn)),
+              chol_cpp_elapsed=rep(NA, length(logn)),
+              chol_icpp_elapsed=rep(NA, length(logn)),
+              nochol_cpp_user=rep(NA, length(logn)),
+              nochol_cpp_elapsed=rep(NA, length(logn)))
+times.n = 1
+
+for (i in 1:length(logn)) {
+  n <- floor(10^logn[i])
+  d <- 1+floor(log(n)) #floor(2+i/3)
+
+  print(n)
+  set.seed(123)
+  X <- matrix(runif(n*d),ncol=d)
+  y <- f(X)
+
+  T = system.time(
+    try(for (j in 1:times.n) r <- bench_rcpp_link(y, X, 1, 2000))
+  ) # 1: user time, 2: system time, 3:elapsed time
+  times$chol_cpp_user[i] <- T[1]
+  times$chol_cpp_elapsed[i] <- T[3]
+  times$chol_icpp_elapsed[i] <- attr(r,'time')
+
+  T = system.time(
+    try(for (j in 1:times.n) r <- bench_rcpp_link(y, X, 2000, 0))
+  ) # 1: user time, 2: system time, 3:elapsed time
+  times$nochol_cpp_user[i] <- T[1]
+  times$nochol_cpp_elapsed[i] <- T[3]
+}
+
+#plot(floor(10^logn),log(times$cpp),ylim=c(log(min(min(times$cpp,na.rm = T),min(times$icpp,na.rm = T))),
+#                                          log(max(max(times$cpp,na.rm = T),max(times$icpp,na.rm = T)))),
+#     xlab="nb points",ylab="log(user_time (s))", panel.first=grid())
+#points(floor(10^logn),log(times$icpp),col='green')
+
+plot(floor(10^logn),times$chol_cpp_elapsed, col='grey', pch=3,
+     ylim=c(min(min(times$chol_cpp_elapsed,na.rm = T),min(times$chol_cpp_user,na.rm = T)),
+            max(max(times$chol_cpp_elapsed,na.rm = T),max(times$chol_cpp_user,na.rm = T))),
+     xlab="nb points",ylab="user_time (s)", panel.first=grid())
+#points(floor(10^logn),times$chol_icpp_elapsed,col='blue')
+points(floor(10^logn),times$chol_cpp_user,col='black')
+points(floor(10^logn),times$nochol_cpp_elapsed,col='orange', pch=3)
+points(floor(10^logn),times$nochol_cpp_user,col='red')
+text(100,1.2,"With Cholesky", col= 'black', pos = 2)
+text(100,1.2,"+ elapsed", col= 'grey', pos = 4)
+text(100,1.0,"Without Cholesky",col = 'red', pos = 2)
+text(100,1.0,"+ elapsed", col= 'orange', pos = 4)
