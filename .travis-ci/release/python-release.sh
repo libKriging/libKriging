aaf6d02c6a36ec8bf0258a556861223a1fc773a2
Pascal Havé <hpwxf@haveneer.com>
2021-08-30 22:04:04 +0200

    Improve pylibkirging generation

--- .travis-ci/release/python-release.sh
+++ .travis-ci/release/python-release.sh
@@ -32,7 +32,6 @@ case $ARCH in
   Darwin|MSYS_NT*|MINGW64_NT*)
     python3 ./bindings/Python/setup.py bdist_wheel
     pip install pylibkriging --no-index -f ./dist
-    # TODO add more tests
     # pytest ./bindings/Python/tests    
     ;;
   *)
---
+++ bindings/Python/src/__init__.py
@@ -0,0 +1,19 @@
+import platform
+
+if platform.system() == "Windows":
+    import os
+    import sys
+
+    shared_lib_path = os.path.join(os.path.dirname(__file__), 'shared_libs')  # cf setup.py
+
+    # alternative method if lib/site-packages prefix is not reliable (requires update of setup.py)
+    # import distutils  # https://docs.python.org/3/distutils/apiref.html#module-distutils.sysconfig
+    # shared_lib_path = os.path.join(distutils.sysconfig.PREFIX, 'pylibkriging', 'shared_libs')
+
+    if sys.version_info.major < 3 or sys.version_info.major == 3 and sys.version_info.minor < 8:  # < 3.8.0
+        sys.path.append(shared_lib_path)
+    else:
+        os.add_dll_directory(shared_lib_path)
+
+from _pylibkriging import *
+from _pylibkriging import __version__, __build_type__
