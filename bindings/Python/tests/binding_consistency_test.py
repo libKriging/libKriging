e0aed245db777be03b775b12cfa07b4fb6f9a46e
Pascal Havé <hpwxf@haveneer.com>
2021-09-02 23:01:28 +0200

    Use PyKriging as reference implementation

--- bindings/Python/tests/binding_consistency_test.py
+++ bindings/Python/tests/binding_consistency_test.py
@@ -60,7 +60,7 @@ def test_data1():
     llgrad_ref = np.genfromtxt(file_llgrad, delimiter=',').reshape(-1, 1)  # FIXME should be a col vec
 
     kernel = "gauss"
-    r = lk.Kriging(y, X, kernel, lk.RegressionModel.Constant, False, "BFGS", "LL", lk.Parameters())
+    r = lk.PyKriging(y, X, kernel, lk.RegressionModel.Constant, False, "BFGS", "LL", lk.Parameters())
     x = 0.3 * np.ones(np.shape(X)[1])
 
     loo, loograd = r.leaveOneOut(x, True)
@@ -86,7 +86,7 @@ def test_data2(i):
     llgrad_ref = np.genfromtxt(file_llgrad, delimiter=',').reshape(-1, 1)  # FIXME should be a col vec
 
     kernel = "gauss"
-    r = lk.Kriging(y, X, kernel)  # use defaults: lk.RegressionModel.Constant, False, "BFGS", "LL", lk.Parameters())
+    r = lk.PyKriging(y, X, kernel)  # use defaults: lk.RegressionModel.Constant, False, "BFGS", "LL", lk.Parameters())
 
     x = 0.3 * np.ones(np.shape(X)[1])
 
