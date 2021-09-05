c120e46ca595919df34998d84116ae495c472610
Pascal Havé <hpwxf@haveneer.com>
2021-09-05 11:52:13 +0200

    Quick fix for release process when R package use static libs

--- .travis-ci/release/r-release.sh
+++ .travis-ci/release/r-release.sh
@@ -37,8 +37,8 @@ case $ARCH in
   Linux)
     ARCHZ="${ARCH}-$(uname -m)"
     tar xzvf "${PREFIX}_${RVER}_R_$(uname -m)-pc-linux-gnu.tar.gz" -C "${TMP_DIR}"
-    cp -a build/installed/lib/libarmadillo.so.* "${TMP_DIR}"/rlibkriging/libs/
-    cp -a build/installed/lib/libKriging.so.* "${TMP_DIR}"/rlibkriging/libs/
+    cp -a build/installed/lib/libarmadillo.so.* "${TMP_DIR}"/rlibkriging/libs/ || echo "No armadillo shared lib to copy"
+    cp -a build/installed/lib/libKriging.so.* "${TMP_DIR}"/rlibkriging/libs/ || echo "No libKriging shared lib to copy"
     RELEASE_FILE=${PREFIX}_${GIT_TAG#v}_${ARCHZ}.tgz
     tar czvf "${RELEASE_FILE}" -C "${TMP_DIR}" .
     ;;
@@ -46,16 +46,16 @@ case $ARCH in
     MACOS_VERSION=$(/usr/libexec/PlistBuddy -c "Print:ProductVersion" /System/Library/CoreServices/SystemVersion.plist)
     ARCHZ="macOS${MACOS_VERSION}-$(uname -m)"
     tar xzvf "${PREFIX}_${RVER}.tgz" -C "${TMP_DIR}"
-    cp -a build/installed/lib/libarmadillo.*.dylib "${TMP_DIR}"/rlibkriging/libs/
-    cp -a build/installed/lib/libKriging.*.dylib "${TMP_DIR}"/rlibkriging/libs/
+    cp -a build/installed/lib/libarmadillo.*.dylib "${TMP_DIR}"/rlibkriging/libs/ || echo "Warning: no armadillo shared lib to copy"
+    cp -a build/installed/lib/libKriging.*.dylib "${TMP_DIR}"/rlibkriging/libs/ || echo "Warning: no libKriging shared lib to copy"
     RELEASE_FILE=${PREFIX}_${GIT_TAG#v}_${ARCHZ}.tgz
     tar czvf "${RELEASE_FILE}" -C "${TMP_DIR}" .
     ;;
   MSYS_NT*|MINGW64_NT*) # Windows
     ARCHZ="$(uname -s | awk -F- '{ print $1$2 }')-$(uname -m)" # remove trailing release
     unzip "${PREFIX}_${RVER}.zip" -d "${TMP_DIR}"
-    cp -a build/installed/lib/libarmadillo.dll "${TMP_DIR}"/rlibkriging/libs/x64/
-    cp -a build/installed/lib/libKriging.dll "${TMP_DIR}"/rlibkriging/libs/x64/
+    cp -a build/installed/lib/libarmadillo.dll "${TMP_DIR}"/rlibkriging/libs/x64/ || echo "Warning: no armadillo shared lib to copy"
+    cp -a build/installed/lib/libKriging.dll "${TMP_DIR}"/rlibkriging/libs/x64/ || echo "Warning: no libKriging shared lib to copy"
     # RELEASE_FILE=${PREFIX}_${ARCHZ}_${GIT_TAG#v}.zip
     RELEASE_FILE="${PREFIX}_${GIT_TAG#v}.zip" # cannot rename for Windows
     (cd "${TMP_DIR}" && zip -FS -r "${RELEASE_FILE}" rlibkriging)
