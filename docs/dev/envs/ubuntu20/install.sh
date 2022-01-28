8061a616361caa920099975f2c4c864a96dcfbe8
Pascal Havé <hpwxf@haveneer.com>
2022-01-28 12:08:44 +0100

    Add Dockerfile to reproduce ubuntu20 environment easily

--- docs/dev/envs/ubuntu20/install.sh
+++ docs/dev/envs/ubuntu20/install.sh
@@ -12,7 +12,7 @@ DEBIAN_FRONTEND="noninteractive" apt install -y tzdata
 # for ubuntu:18, the equivalent of liboctave-dev was octave-pkg-dev 
 apt install -y build-essential g++ cmake git python3 python3-pip octave liboctave-dev r-base liblapack-dev gfortran
 apt install -y lcov valgrind # advanced tools
-apt install -y ccache vim curl # convenient tools
+apt install -y ccache ninja-build vim curl # convenient tools
 
 # only required for ubuntu:18
 ## commands from linux-macos/install.sh
