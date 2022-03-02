4fa8081d5f2e70493e0c8ab61f1efbcdbf32dec5
yannrichet <richetyann@gmail.com>
2022-03-02 18:25:03 +0100

    ...

--- src/lib/include/libKriging/Kriging.hpp
+++ src/lib/include/libKriging/Kriging.hpp
@@ -94,7 +94,7 @@ class Kriging {
   bool m_est_theta;
   double m_sigma2;
   bool m_est_sigma2;
-
+  
   std::function<double(const arma::vec&)>
       CovNorm_fun;  // dist_norm is L1 distance between to points of X, divided by theta
   std::function<arma::vec(const arma::vec&)> Dln_CovNorm;
