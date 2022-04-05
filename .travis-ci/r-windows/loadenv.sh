2ec39dad375c4c4203d9345cbf87bd54aa458b9f
Pascal Havé <hpwxf@haveneer.com>
2022-04-05 17:24:38 +0200

    Improve R detection tools for Windows

--- .travis-ci/r-windows/loadenv.sh
+++ .travis-ci/r-windows/loadenv.sh
@@ -1,19 +1,13 @@
 # make
 export PATH="/c/Program Files/make/make-4.3/bin":${PATH}
 
-
-# Using R from Chocolatey
-# R shortcut (and tools with absolute path) by Chocolatey
-export PATH="/c/Program Files/R/R-3.6.0/bin":$PATH
-# Required to disambiguate with Chocolatey GCC installation
-export PATH="/c/Rtools/mingw_64/bin":$PATH
-
+REXE=$(which R 2>/dev/null) || REXE=$(find "/c/Program Files" -wholename "*/bin/x64/R.exe")
+RDIR=$(dirname "$REXE")
+export PATH="$RDIR":$PATH
 
 # Using R from Anaconda
 ## R shortcut (and tools) by Anaconda
 #export PATH=${HOME}/Miniconda3/Scripts:${PATH}
-#export PATH=$HOME/Miniconda3/Rtools/mingw_64/bin:$PATH
-
 
 # In all cases, we need an access to libomp.dll, flang.dll, flangrti.dll, openblas.dll
 export PATH=${HOME}/Miniconda3/Library/bin:$PATH
