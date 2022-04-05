159e867e80c29ef9ea5b4939101fe43cead48385
yannrichet <richetyann@gmail.com>
2022-04-05 12:44:46 +0200

    clang-format

--- bindings/R/rlibkriging/src/kriging_binding.cpp
+++ bindings/R/rlibkriging/src/kriging_binding.cpp
@@ -31,7 +31,7 @@ Rcpp::List new_Kriging(arma::vec y,
       _parameters.push_back(!(params.containsElementNamed("estim_sigma2") && !params["estim_sigma2"]), "estim_sigma2");
     } else {
       //_parameters.push_back(Rcpp::runif(1), "sigma2"); // turnaround mingw bug:
-      // https://github.com/msys2/MINGW-packages/issues/5019 _parameters.push_back(true, "has_sigma2");
+      //https://github.com/msys2/MINGW-packages/issues/5019 _parameters.push_back(true, "has_sigma2");
       _parameters.push_back(-1, "sigma2");
       _parameters.push_back(false, "has_sigma2");
       _parameters.push_back(true, "estim_sigma2");
@@ -43,7 +43,7 @@ Rcpp::List new_Kriging(arma::vec y,
     } else {
       // Rcpp::NumericVector r = Rcpp::runif(X.n_cols); // turnaround mingw bug:
       // https://github.com/msys2/MINGW-packages/issues/5019 _parameters.push_back(Rcpp::NumericMatrix(1, X.n_cols,
-      // r.begin()), "theta"); _parameters.push_back(true, "has_theta");
+      //r.begin()), "theta"); _parameters.push_back(true, "has_theta");
       _parameters.push_back(Rcpp::NumericVector(0), "theta");
       _parameters.push_back(false, "has_theta");
       _parameters.push_back(true, "estim_theta");
