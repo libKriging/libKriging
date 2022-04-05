4ee1aa683b57d06014264f178f3730e4471013c0
yannrichet <richetyann@gmail.com>
2022-04-05 19:58:21 +0200

    should fix matlab failure

--- bindings/Octave/NuggetKriging_binding.hpp
+++ bindings/Octave/NuggetKriging_binding.hpp
@@ -4,15 +4,15 @@
 #include <mex.h>
 
 namespace NuggetKrigingBinding {
-void build(int nlhs, void** plhs, int nrhs, const void** prhs);
-void destroy(int nlhs, void** plhs, int nrhs, const void** prhs);
-void fit(int nlhs, void** plhs, int nrhs, const void** prhs);
-void predict(int nlhs, void** plhs, int nrhs, const void** prhs);
-void simulate(int nlhs, void** plhs, int nrhs, const void** prhs);
-void update(int nlhs, void** plhs, int nrhs, const void** prhs);
-void summary(int nlhs, void** plhs, int nrhs, const void** prhs);
-void logLikelihood(int nlhs, void** plhs, int nrhs, const void** prhs);
-void logMargPost(int nlhs, void** plhs, int nrhs, const void** prhs);
+void build(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void destroy(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void fit(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void predict(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void simulate(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void update(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void summary(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void logLikelihood(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
+void logMargPost(int nlhs, mxArray** plhs, int nrhs, const mxArray** prhs);
 }  // namespace NuggetKrigingBinding
 
 #endif  // LIBKRIGING_BINDINGS_OCTAVE_NUGGETKRIGING_BINDING_HPP
