ef33430b70eaff70d6888063dcd007356c3db03e
yannrichet <richetyann@gmail.com>
2022-05-06 21:01:43 +0200

    user bool warn_chol to setup verbosity when fix chol

--- src/lib/include/libKriging/LinearAlgebra.hpp
+++ src/lib/include/libKriging/LinearAlgebra.hpp
@@ -8,9 +8,12 @@
 class LinearAlgebra {
  public:
   static const arma::solve_opts::opts default_solve_opts;
+
   static double num_nugget;
   LIBKRIGING_EXPORT static void set_num_nugget(double nugget);
   LIBKRIGING_EXPORT static double get_num_nugget();
+  
+  static bool warn_chol;
   LIBKRIGING_EXPORT static arma::mat safe_chol_lower(arma::mat X);
   static arma::mat safe_chol_lower(arma::mat X, int warn);
 };
