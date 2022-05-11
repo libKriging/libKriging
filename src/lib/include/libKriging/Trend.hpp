8158ffc1df008e276f9007456633ba5da074c445
yannrichet <richetyann@gmail.com>
2022-05-11 14:58:43 +0200

    remove ambiguity on out size: id. to newXt

--- src/lib/include/libKriging/Trend.hpp
+++ src/lib/include/libKriging/Trend.hpp
@@ -15,9 +15,7 @@ class Trend {
   LIBKRIGING_EXPORT static std::string toString(const RegressionModel& m);
 
   LIBKRIGING_EXPORT static arma::mat regressionModelMatrix(const RegressionModel& regmodel,
-                                                           const arma::mat& newX,
-                                                           arma::uword n,
-                                                           arma::uword d);
+                                                           const arma::mat& newXt);
 };
 
 #endif  // LIBKRIGING_SRC_LIB_INCLUDE_LIBKRIGING_TREND_HPP
