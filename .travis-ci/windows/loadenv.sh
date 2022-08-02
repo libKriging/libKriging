6dbeeda16e273929366ffbac2d11445ee2fd51bc
Pascal Havé <hpwxf@haveneer.com>
2022-08-02 21:54:14 +0200

    Add helper for loading CMake for manual build

--- .travis-ci/windows/loadenv.sh
+++ .travis-ci/windows/loadenv.sh
@@ -6,5 +6,16 @@
   #export PATH=${HOME}/Miniconda3:$PATH
 # fi
 
+# CMake helper
+if ( ! command -v cmake >/dev/null 2>&1 ); then
+  CMAKE_EXE=$(find "/c/Program Files/CMake" -name "cmake.exe" |& grep -v "Permission denied") || true
+  if [ -z "$CMAKE_EXE" ]; then
+    echo "Cannot find CMake executable" >&2
+    exit 1
+  fi
+  CMAKE_DIR=$(dirname "$CMAKE_EXE")
+  export PATH="$CMAKE_DIR":$PATH
+fi
+
 # add OpenBLAS DLL library path
 export PATH=$HOME/Miniconda3/Library/bin:$PATH
