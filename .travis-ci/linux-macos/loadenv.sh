f0352a740644f87212a427b9c6319afe8ec472a1
Pascal Havé <hpwxf@haveneer.com>
2023-01-09 11:33:38 +0100

    Add workaround to find gfortran

--- .travis-ci/linux-macos/loadenv.sh
+++ .travis-ci/linux-macos/loadenv.sh
@@ -1,3 +1,11 @@
-if [ "$(uname -s)" == "Darwin" ]; then
-  export PATH=$(brew --prefix gfortran)/bin:$PATH
+if ( command -v gfortran >/dev/null 2>&1 ); then
+    # echo "gfortran found in PATH"
+    : # do nothing
+elif ( command -v R >/dev/null 2>&1 ); then
+    # echo "R will get its fortran compiler"
+    Ftmp=$(R CMD config FC)
+    export PATH=$PATH:$(cd $(dirname -- $Ftmp) && pwd -P)
+elif [ "$(uname -s)" == "Darwin" ]; then
+    # echo "Ask to brew package system
+    export PATH=$(brew --prefix gfortran)/bin:$PATH
 fi
