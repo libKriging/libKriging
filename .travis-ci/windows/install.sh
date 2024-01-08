a3f486952f06d4e15b253dee38d1e27cb5c1aa4a
Pascal Havé <hpwxf@haveneer.com>
2024-01-08 09:26:53 +0100

    Add missing pkg-config in installation script for Windows CI

--- .travis-ci/windows/install.sh
+++ .travis-ci/windows/install.sh
@@ -23,7 +23,7 @@ fi
 $HOME/Miniconda3/condabin/conda.bat update -y -n base -c defaults conda
 
 # https://anaconda.org/search?q=blas
-$HOME/Miniconda3/condabin/conda.bat install -y --quiet -n base -c conda-forge openblas liblapack # hdf5
+$HOME/Miniconda3/condabin/conda.bat install -y --quiet -n base -c conda-forge openblas liblapack pkg-config # hdf5
 
 # https://chocolatey.org/docs/commands-install
 # required to compile fortran part
