decc1999d9327a29b291a2409d9381f4f55f29ee
yannrichet <richetyann@gmail.com>
2024-05-04 17:05:49 +0200

    add canary for octave plot

---
+++ bindings/Octave/tests/canary_plot.m
@@ -0,0 +1,18 @@
+% clear all
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0
+
+X = [0.0;0.2;0.5;0.8;1.0];
+f = @(x) 1-1/2.*(sin(12*x)./(1+x)+2*cos(7.*x).*x.^5+0.7)
+y = f(X);
+
+% session
+x = reshape(0:(1/99):1,100,1);
+if (isOctave)
+    h = figure(1, 'Visible','off'); % no display
+else
+    h = figure(1);       
+    h.Visible = 'off'; % no display
+end
+hold on;
+plot(x,f(x));
+scatter(X,f(X));
