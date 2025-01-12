d297b5cb3c95d1f225cd1e5aa1cefe8c196d968d
Yann Richet <yann.richet@irsn.fr>
2025-01-12 12:43:13 +0100

    enable static_lib=on everywhere

--- .travis-ci/windows/build.sh
+++ .travis-ci/windows/build.sh
@@ -19,15 +19,14 @@ BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR" && pwd -P)
 test -f "${BASEDIR}"/loadenv.sh && . "${BASEDIR}"/loadenv.sh 
 
-STATIC_LIB=on
-MAKE_SHARED_LIBS=off
-
 # OpenBLAS installation
 export EXTRA_SYSTEM_LIBRARY_PATH=${HOME}/Miniconda3/Library/lib
 
 # mwblas and mwlapack are provided by Matlab/extern on Windows
 # export EXTRA_SYSTEM_LIBRARY_PATH="C:/Program Files/MATLAB/R2022a/extern/lib/win64/microsoft"
 # EXTRA_CMAKE_OPTIONS="${EXTRA_CMAKE_OPTIONS} -DBLAS_NAMES=libmwblas -DLAPACK_NAMES=libmwlapack"
+STATIC_LIB=on
+MAKE_SHARED_LIBS=off
 EXTRA_CMAKE_OPTIONS="-DBUILD_SHARED_LIBS=${MAKE_SHARED_LIBS} -DSTATIC_LIB=${STATIC_LIB}"
 
 mkdir -p ${BUILD_DIR:-build}
