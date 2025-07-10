4dc34327323b08c420a706bc3a1dd13886ee2e74
yannrichet <richetyann@gmail.com>
2025-07-10 21:01:00 +0200

    if () {cout} instead of if () cout

--- src/lib/NuggetKriging.cpp
+++ src/lib/NuggetKriging.cpp
@@ -293,8 +293,9 @@ LIBKRIGING_EXPORT std::tuple<double, arma::vec> NuggetKriging::logLikelihoodFun(
     size_t num = 0;
     for (auto& kv : bench)
       num = std::max(kv.first.size(), num);
-    for (auto& kv : bench)
+    for (auto& kv : bench) {
       arma::cout << "| " << Bench::pad(kv.first, num, ' ') << " | " << kv.second << " |" << arma::endl;
+    }
 
   } else {
     if (_grad) {
