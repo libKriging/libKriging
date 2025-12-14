c55b0470c4ee339ddef9341bd28b8faa2ec00764
yannrichet <richetyann@gmail.com>
2025-12-14 12:27:36 +0100

    Use Octave's MinGW for Windows Octave CI

--- .travis-ci/octave-windows/install.sh
+++ .travis-ci/octave-windows/install.sh
@@ -9,7 +9,21 @@ BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR" && pwd -P)
 test -f "${BASEDIR}"/loadenv.sh && . "${BASEDIR}"/loadenv.sh 
 
-"${BASEDIR}"/../windows/install.sh
+if [[ "${ENABLE_COVERAGE}" == "on" ]]; then
+    echo "Coverage not supported for Windows"
+    travis_terminate 1
+fi
+
+# Install Miniconda for BLAS/LAPACK dependencies
+if [ ! -f "$HOME/Miniconda3/condabin/conda.bat" ]; then
+  echo "Installing Miniconda (provides BLAS/LAPACK)..."
+  curl --insecure -s -o "${HOME}"/Downloads/Miniconda3-latest-Windows-x86_64.exe https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+  pushd "${HOME}"/Downloads
+  "${BASEDIR}"/../windows/install_conda.bat
+  popd
+fi
+$HOME/Miniconda3/Scripts/conda.exe update -y -n base -c defaults conda
+$HOME/Miniconda3/Scripts/conda.exe install -y --quiet -n base -c conda-forge openblas liblapack pkg-config
 
 # https://chocolatey.org/docs/commands-install
 # https://chocolatey.org/packages/make
@@ -19,6 +33,10 @@ if [[ "$ENABLE_OCTAVE_BINDING" == "on" ]]; then
   # select your version from https://community.chocolatey.org/packages/octave.portable#versionhistory
   # Don't forget to update loadenv.sh with the related path
   choco install -y --no-progress octave.portable --version=6.2.0
+  
+  # Note: We use Octave's built-in MinGW compiler instead of installing a separate mingw package
+  # This avoids conflicts between different MinGW versions
+  
   if [[ ! -e  /c/windows/system32/GLU32.DLL ]]; then
     # add missing GLU32.dll in travis-ci windows image
     # 64bit 10.0.14393.0	161.5 KB	U.S. English	OpenGL Utility Library DLL
